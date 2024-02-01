b2a046cc1ed50e64c617e698cb283eb043575082
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2024-02-01 20:31:20 +0700

    [Implement][EF-85-87][Update API management view order, decrease quantity product when order is created]

---
+++ src/order/controllers/provider.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, UseGuards } from '@nestjs/common'
+import { ApiBearerAuth, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger'
+import * as _ from 'lodash'
+
+import { PaginationQuery } from '@common/contracts/dto'
+import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard'
+import { RolesGuard } from '@auth/guards/roles.guard'
+import { UserRole } from '@common/contracts/constant'
+import { Roles } from '@auth/decorators/roles.decorator'
+import { OrderService } from '@order/services/order.service'
+import { Pagination, PaginationParams } from '@common/decorators/pagination.decorator'
+import { OrderResponseDto } from '@order/dto/order.dto'
+
+@ApiTags('Order - Provider')
+@ApiBearerAuth()
+@Roles(UserRole.ADMIN, UserRole.STAFF)
+@UseGuards(JwtAuthGuard.ACCESS_TOKEN, RolesGuard)
+@Controller('provider')
+export class OrderProviderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Get()
+  @ApiOkResponse({ type: OrderResponseDto })
+  @ApiQuery({ type: PaginationQuery })
+  async getListOrder(@Pagination() paginationParams: PaginationParams) {
+    return await this.orderService.getOrderList({  }, paginationParams)
+  }
+}
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -6,10 +6,12 @@ import { OrderCustomerController } from '@src/order/controllers/customer.control
 import { OrderService } from '@order/services/order.service'
 import { OrderRepository } from '@order/repositories/order.repository'
 import { CartModule } from '@cart/cart.module'
+import { ProductModule } from '@product/product.module'
+import { OrderProviderController } from '@order/controllers/provider.controller'
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]), CartModule],
-  controllers: [OrderCustomerController],
+  imports: [MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]), CartModule, ProductModule],
+  controllers: [OrderCustomerController, OrderProviderController],
   providers: [OrderService, OrderRepository],
   exports: [OrderService]
 })
