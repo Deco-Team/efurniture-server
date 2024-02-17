451eda99e4bec4ff11206e46126b2329f3bb22a3
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2024-02-17 16:27:37 +0700

    [Implement][EF-154]API update staff

--- src/staff/dto/staff.dto.ts
+++ src/staff/dto/staff.dto.ts
@@ -1,49 +1,57 @@
-import { ApiProperty } from '@nestjs/swagger'
+import { ApiProperty, PartialType, PickType } from '@nestjs/swagger'
 import { DataResponse, PaginateResponse } from '@src/common/contracts/openapi-builder'
 import { IsEmail, IsEnum, IsNotEmpty, IsPhoneNumber, IsUrl, MaxLength } from 'class-validator'
 import { StaffRole } from '@src/common/contracts/constant'
 import { Staff } from '@staff/schemas/staff.schema'
+import { Types } from 'mongoose'
 
 export class CreateStaffDto {
-    @ApiProperty({ example: 'Staff'})
-    @IsNotEmpty()
-    @MaxLength(30)
-    firstName: string
-  
-    @ApiProperty({ example: 'Name' })
-    @IsNotEmpty()
-    @MaxLength(30)
-    lastName: string
+  @ApiProperty({ example: 'Staff' })
+  @IsNotEmpty()
+  @MaxLength(30)
+  firstName: string
 
-    @ApiProperty({ example: 'EF123456' })
-    @IsNotEmpty()
-    @MaxLength(30)
-    staffCode: string
+  @ApiProperty({ example: 'Name' })
+  @IsNotEmpty()
+  @MaxLength(30)
+  lastName: string
 
-    @ApiProperty({ example: '0987654321' })
-    @IsNotEmpty()
-    @IsPhoneNumber('VN')
-    phone: string
+  @ApiProperty({ example: 'EF123456' })
+  @IsNotEmpty()
+  @MaxLength(30)
+  staffCode: string
 
-    @ApiProperty({ example: 'https://i.stack.imgur.com/l60Hf.png' })
-    @IsNotEmpty()
-    @IsUrl()
-    avatar: string
-  
-    @ApiProperty({ example: 'staff@gmail.com' })
-    @IsNotEmpty()
-    @IsEmail()
-    email: string
+  @ApiProperty({ example: '0987654321' })
+  @IsNotEmpty()
+  @IsPhoneNumber('VN')
+  phone: string
 
-    @ApiProperty({ example: 'STAFF | DELIVERY_STAFF | CONSULTANT_STAFF' })
-    @IsNotEmpty()
-    @IsEnum(StaffRole)
-    role: StaffRole
+  @ApiProperty({ example: 'https://i.stack.imgur.com/l60Hf.png' })
+  @IsNotEmpty()
+  @IsUrl()
+  avatar: string
 
-    password?: string
-    providerId?: string
-    createdBy?: string
-  }
+  @ApiProperty({ example: 'staff@gmail.com' })
+  @IsNotEmpty()
+  @IsEmail()
+  email: string
+
+  @ApiProperty({ example: 'STAFF | DELIVERY_STAFF | CONSULTANT_STAFF' })
+  @IsNotEmpty()
+  @IsEnum(StaffRole)
+  role: StaffRole
+
+  password?: string
+  providerId?: Types.ObjectId
+  createdBy?: string
+}
+
+export class UpdateStaffDto extends PickType(PartialType(CreateStaffDto), [
+  'firstName',
+  'lastName',
+  'phone',
+  'avatar'
+]) {}
 
 export class StaffPaginateResponseDto extends DataResponse(
   class StaffPaginateResponse extends PaginateResponse(Staff) {}
